f4993f009fdbbd26c873437ff28eabb18c9c5b22
Nikolina Kokor <nkokor@icloud.com>
2023-11-07 18:21:42 +0100

    rb_delete and rb_delete_fixup methods implemented

--- RBTree.py
+++ RBTree.py
@@ -98,3 +98,76 @@ class RBTree():
     else:
       u.p.right = v
     v.p = u.p
+
+  def rb_delete_fixup(self, x):
+    while x != self.root and x.color == RBColor.BLACK:
+      if x == x.p.left:
+        w = x.p.right
+        if w.color == RBColor.RED:
+          w.color = RBColor.BLACK
+          x.p.color = RBColor.RED
+          self.left_rotate(x.p)
+          w = x.p.right
+        if w.left.color == RBColor.BLACK and w.right.color == RBColor.BLACK:
+          w.color = RBColor.RED
+          x = x.p
+        else:
+          if w.right.color == RBColor.BLACK:
+            w.left.color = RBColor.BLACK
+            w.color = RBColor.RED
+            self.right_rotate(w)
+            w = x.p.right
+          w.color = x.p.color
+          x.p.color = RBColor.BLACK
+          w.right.color = RBColor.BLACK
+          self.left_rotate(x.p)
+          x = self.root
+      else:
+        w = x.p.left
+        if w.color == RBColor.RED:
+          w.color = RBColor.BLACK
+          x.p.color = RBColor.RED
+          self.right_rotate(x.p)
+          w = x.p.left
+        if w.right.color == RBColor.BLACK and w.left.color == RBColor.BLACK:
+          w.color = RBColor.RED
+          x = x.p
+        else:
+          if w.left.color == RBColor.BLACK:
+            w.right.color = RBColor.BLACK
+            w.color = RBColor.RED
+            self.left_rotate(w)
+            w = x.p.left
+          w.color = x.p.color
+          x.p.color = RBColor.BLACK
+          w.left.color = RBColor.BLACK
+          self.right_rotate(x.p)
+          x = self.root
+    x.color = RBColor.BLACK
+
+  def rb_delete(self, z):
+    y = z
+    y_original_color = y.color
+    if z.left == self.tnil:
+      x = z.right
+      self.rb_transplant(z, z.right)
+    elif z.right == self.tnil:
+      x = z.left
+      self.rb_transplant(z, z.left)
+    else:
+      y = self.tree_minimum(z.right)
+      y_original_color = y.color
+      x = y.right
+      if y != z.right:
+        self.rb_transplant(y, y.right)
+        y.right = z.right
+        y.right.p = y
+      else: 
+        x.p = y
+      self.rb_transplant(z, y)
+      y.left = z.left
+      y.left.p = y
+      y.color = z.color
+    if y_original_color == RBColor.BLACK:
+      self.rb_delete_fixup(x)
+    
