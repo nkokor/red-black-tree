05b9908f1ff00b726196f8054feb162a94b87989
Nikolina Kokor <nkokor@icloud.com>
2023-11-07 17:23:57 +0100

    left_rotate and right_rotate methods implemented

--- RBTree.py
+++ RBTree.py
@@ -7,3 +7,32 @@ class RBTree():
     self.root = self.TNIL
     self.n = 0
 
+  def left_rotate(self, x):
+    y = x.right
+    x.right = y.left
+    if y.left != self.tnil:
+      y.left.p = x
+    y.p = x.p
+    if x.p == self.tnil:
+      self.root = y
+    elif x == x.p.left:
+      x.p.left = y
+    else:
+      x.p.right = y
+    y.left = x
+    x.p = y
+
+  def right_rotate(self, x):
+    y = x.left
+    x.left = y.right
+    if y.right != self.tnil:
+      y.right.p = x
+    y.p = x.p
+    if x.p == self.tnil:
+      self.root = y
+    elif x == x.p.left:
+      x.p.left = y
+    else:
+      x.p.right = y
+    y.right = x
+    x.p = y
