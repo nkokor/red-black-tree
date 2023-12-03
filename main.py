45f3251f9c6c8798f892cd2fdf61bbba0b30a53f
Nikolina Kokor <nkokor@icloud.com>
2023-12-03 13:03:31 +0100

    imports fixed

--- main.py
+++ main.py
@@ -11,26 +11,32 @@ def run_app():
     print("3 - Delete node")
     print("4 - Exit")
     print("Input option number: ")
-    option = int(input())
-    if option == 4:
-      running = False
-    else:
-      if option == 1:
-        print("Input key value: ")
-        key = int(input())
-        new_node = RBNode(key)
-        rb_tree.rb_insert(new_node)
-      elif option == 2:
-        print(rb_tree.get_inorder(rb_tree.root))
-      elif option == 3:
-        print("Input key to delete: ")
-        key = int(input())
-        node = rb_tree.get_node_with_key(key)
-        if node != None:
-          rb_tree.rb_delete(node)
+    try:
+      user_input = input().strip()
+      if user_input:
+        option = int(user_input)
+        if option == 4:
+          running = False
         else:
-          print("No node with such value found in tree!")
-      else:
-        print("Wrong input!")
+          if option == 1:
+            print("Input key value: ")
+            key = int(input())
+            new_node = RBNode(key)
+            rb_tree.rb_insert(new_node)
+          elif option == 2:
+            print(rb_tree.rb_inorder_traversal(rb_tree.root))
+          elif option == 3:
+            print("Input key to delete: ")
+            key = int(input())
+            node = rb_tree.get_node_with_key(key)
+            if node != None:
+              rb_tree.rb_delete(node)
+            else:
+              print("No node with such value found in tree!")
+          else:
+            print("Wrong input!")
+    except ValueError:
+      print("Invalid input. Please enter a valid integer.")
+      continue
 
 run_app()
--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,11 @@
+import os
+import sys
 import unittest
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+classes_dir = os.path.join(current_dir, "..") 
+sys.path.append(classes_dir)
+
 from classes.RBTree import RBTree
 from classes.RBNode import RBNode
 
